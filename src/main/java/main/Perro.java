c25141c4fc6c3d6780ed6c6f8e885e51e2645368
Lucas Domenica <lucasgye.3325@gmail.com>
2021-10-15 12:45:56 -0300

    clase perro agregada

---
+++ src/main/java/main/Perro.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author BETH
+ */
+public class Perro {
+     private String nombre;
+    private String raza;
+    private Integer edad;
+}
