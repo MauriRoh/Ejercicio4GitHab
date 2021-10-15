c8363ae65f7fc0b1f2a19c486244d823ae282bcc
MauriRoh <mauriroh@gmail.com>
2021-10-15 12:22:45 -0300

    2do commit - getter and setter

--- src/main/java/main/Caballo.java
+++ src/main/java/main/Caballo.java
@@ -10,6 +10,39 @@ public class Caballo {
     private String Id;
     private String nombre;
     private String Descendencia;
+
+    public Caballo() {
+    }
+
+    public Caballo(String Id, String nombre, String Descendencia) {
+        this.Id = Id;
+        this.nombre = nombre;
+        this.Descendencia = Descendencia;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescendencia() {
+        return Descendencia;
+    }
+
+    public void setDescendencia(String Descendencia) {
+        this.Descendencia = Descendencia;
+    }
     
     
 }
